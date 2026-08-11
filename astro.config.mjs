87b7d81099152b1d65aacfd89e1e88959329e910
Bruno Cunha Lima <cunhalimabruno@gmail.com>
2026-08-11 18:14:38 -0300

    Fix GitHub Pages base path detection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 
 const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isUserPage = repositoryName?.endsWith(".github.io");
+const repositoryOwner = process.env.GITHUB_REPOSITORY?.split("/")[0];
+const isUserPage =
+  repositoryName?.toLowerCase() === `${repositoryOwner?.toLowerCase()}.github.io`;
 const base = process.env.PUBLIC_BASE_PATH ?? (repositoryName && !isUserPage ? `/${repositoryName}` : "/");
 const site = process.env.PUBLIC_SITE_URL;
 
